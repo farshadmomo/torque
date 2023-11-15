2993dc5c4199af9aee01cb077951990666749fe3
farshadmomo <momtazfarshad@gmail.com>
2023-11-15 15:36:58 +0330

    gozaresh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header";
 import Inputs from "./components/Inputs";
+import EjraGozaresh from "./components/EjraGozaresh";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Inputs/>
+            <EjraGozaresh />
         </div>
     );
 }
---
+++ src/components/EjraGozaresh.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function EjraGozaresh() {
+        return (
+            <div className='gozaresh-container'>
+                <div className='gozaresh-child-container'>
+                    <div>موقعیت اندازه گیری:</div>
+                    <div>نوع چک لیست:</div>
+                    <div>اتصال:</div>
+                </div>
+                <div className='gozaresh-child-container'>
+                    <div>موقعیت اندازه گیری:</div>
+                    <div>نوع چک لیست:</div>
+                </div>
+            </div>
+        );
+    }
